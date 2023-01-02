9075dd062db65f9bf72fe758f4e324c553edfaad
Shubham3023 <shubhamverma3023@gmail.com>
2023-01-02 13:06:46 +0530

    added logging and exception handeling to the code

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os, sys
+from exception import SensorException
+from logger import logging
 
 ### loading model and scalar object
+logging.info("Reading model object from model.sav file")
 model=joblib.load('model_web_etc.sav')
+logging.info("Reading transformer object from scaler.sav file")
 scalar=joblib.load('scalar_web.sav')
 
 app=Flask(__name__)
 
 ### creating home route
+logging.info("Creating Home route")
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
 # Creating Prediction route
+logging.info("Creating Prediction route")
 @app.route('/prediction',methods=['POST'])
 def prediction():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    output=model.predict(final_input)[0]
-    #print(output)
-    if output==1:
-        output= "Tumor is Malignant. Your diagnosis is Positive."
-    else:
-        output="Tumor is Benign. Your diagnosis is Negative."
-    return render_template("home.html",prediction_value="Model Prediction: {}".format(output))
+    try:
+        logging.info("Getting data from the web form for prediction")
+        data=[float(x) for x in request.form.values()]
+        logging.info("Appling standard scaling to input data and reshaping data for single prediction")
+        final_input=scalar.transform(np.array(data).reshape(1,-1))
+        logging.info("Passing input to model and getting prediction")
+        output=model.predict(final_input)[0]
+        #print(output)
+        if output==1:
+            logging.info("Model Prediction is: Tumor is Malignant. Your diagnosis is Positive.")
+            output= "Tumor is Malignant. Your diagnosis is Positive."
+        else:
+            logging.info("Model Prediction is: Tumor is Benign. Your diagnosis is Negative.")
+            output="Tumor is Benign. Your diagnosis is Negative."
+        logging.info("Returning model prediction to web application")
+        return render_template("home.html",prediction_value="Model Prediction: {}".format(output))
+    except Exception as e:
+        raise SensorException(e, sys)
+
 
 if __name__=='__main__':
     app.run()
